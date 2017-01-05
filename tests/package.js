0d4e0042f94df34f5cc1ba27291bb33d5996a0f3
Gabriel Imre <gabrielimre@work.local>
2017-01-05 13:56:48 +0200

    added package.js test descriptions

--- tests/package.js
+++ tests/package.js
@@ -1,8 +1,29 @@
 
 const { expect } = require( 'chai' )
+const ioc        = require( 'electrolyte' )
+
+// init DI
+ioc.use( ioc.node_modules( ) )
+ioc.use( ioc.dir( './src' ) )
 
 describe( 'Package', ( ) => {
-    it( 'should work good', ( ) => {
-        expect( true ).to.equal( true )
+    let instance
+
+    before( ( ) => {
+        const Package = ioc.create( './package' )
+        instance = new Package( ) 
     } )
+
+    it( 'should be an object', ( ) => {
+        expect( instance ).to.be.an( 'object' )
+    } )
+
+    it( 'should be a stream', ( ) => {
+        const { Stream } = require( 'stream' )
+        expect( instance ).to.be.an.instanceOf( Stream )
+    } )
+
+    it( 'should be an extractable zip archive' )
+    it( 'should contain the META-INF folder' )
+    it( 'should contain the META-INF/filter.xml file' )
 } )
